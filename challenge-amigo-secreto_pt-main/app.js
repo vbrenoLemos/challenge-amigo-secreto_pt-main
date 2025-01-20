f26a3e1fa9ae2a37f0fdafefcd3ead2cfbc8233b
Breno Lemos <brenolemos327@gmail.com>
2025-01-20 19:29:08 -0300

    first commit

---
+++ challenge-amigo-secreto_pt-main/app.js
@@ -0,0 +1,35 @@
+//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. 
+// Aqui você deverá desenvolver a lógica para resolver o problema.
+
+// Para o sorteio do amigo posso acessar o indice do array usando Math.random, e parar limitar ate quanto 
+// ele vai sortear, uso uma variavel que vai receber o length do array 
+
+let amigos = [];
+
+function adicionarAmigo(){
+    const input = document.getElementById('amigo');
+
+
+    if(input.value.trim() === ''){
+        alert("Por favor, insira um nome.")
+    }
+    else{
+    let amigo = input.value.trim();
+    amigos.push(amigo)
+
+    let table = document.getElementById('listaAmigos')
+    table.innerHTML += amigo + "<br>"
+
+    input.value = '';
+    }  
+}
+
+function sortearAmigo(){
+    let qntAmigos = amigos.length
+
+    let numeroSortido = parseInt(Math.random() * qntAmigos + 1)
+
+    let result = document.getElementById('resultado');
+
+    result.innerHTML = amigos[numeroSortido]
+}
